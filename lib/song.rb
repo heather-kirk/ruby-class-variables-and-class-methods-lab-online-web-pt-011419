818a6c86a262c45e286fb8a52d5e9ddf725fdd3d
Heather Kirk <hskirk@gmail.com>
2019-02-07 22:19:38 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,40 @@
+class Song 
+  attr_accessor :name, :artist, :genre
+  
+  @@count = 0 
+  @@genres = []
+  @@artists = []
+  
+  def initialize (name, artist, genre)
+    @name = name
+    @genre = genre
+    @artist = artist
+    @@count +=1 
+    @@genres << genre
+    @@artists << artist 
+  end 
+  
+  def self.count
+    @@count
+  end 
+  
+  def self.genres
+  @@genres.uniq
+  end 
+  
+  def self.artists
+    @@artists.uniq
+  end 
+  
+  def self.genre_count
+    arr = {}
+    @@genres.each do |genre|
+    if arr[genre]
+     arr[genre] +=1
+    else 
+    arr[genre] = 1 
+      end 
+    end 
+  end 
+  arr[genre]
+end 
